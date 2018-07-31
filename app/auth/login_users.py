d38a16246aae0b819fedea254f7cc032cf1c9f60
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-31 15:28:14 +0300

    Аутентификация с помощью api_key (параметр запроса и basic access authentication http)

--- app/auth/login_users.py
+++ app/auth/login_users.py
@@ -1,6 +1,7 @@
 import flask_login
 from passlib.hash import pbkdf2_sha256
 import logging
+import base64
 
 from .models import UsersReport
 
@@ -10,17 +11,40 @@ login_manager.login_view = 'auth.login'
 
 
 @login_manager.user_loader
-def user_loader(login):
-    logging.debug('user_loader')
-    user_report = UsersReport.query.filter(UsersReport.login == login).first()
+def user_loader(id):
+    logging.debug('user_loader: {}'.format(id))
+    user_report = UsersReport.query.filter(UsersReport.id == id).first()
     if not user_report:
         return None
     return user_report
 
 
+def get_user_with_api_key(api_key):
+    if not api_key:
+        return
+    return UsersReport.query.filter_by(api_key=api_key).first()
+
+
 @login_manager.request_loader
 def request_loader(request):
     logging.debug('request_loader')
+    # first, try to login using the api_key url arg
+    api_key = request.args.get('api_key')
+    user = get_user_with_api_key(api_key)
+    if user:
+        return user
+    # next, try to login using Basic Auth
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Basic ', '', 1)
+        try:
+            api_key = base64.b64decode(api_key)
+        except TypeError:
+            pass
+    user = get_user_with_api_key(api_key)
+    if user:
+        return user
+    # next, try to login using form
     login = request.form.get('login')
     password = request.form.get('password')
     user_report = UsersReport.query.filter(UsersReport.login == login).first()
